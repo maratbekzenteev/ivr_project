cfc4bb1af2c33bef43984e988ce7647f2aae1733
marat <maratbekzenteev@yandex.ru>
2023-06-30 13:06:31 +0300

    added a primitive grid layout

--- main.py
+++ main.py
@@ -2,39 +2,29 @@ import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel, QGraphicsScene, QGraphicsView, QGraphicsItem,
                              QDockWidget, QWidget, QListWidget, QPushButton, QGridLayout, QShortcut)
 from PyQt5.QtGui import QImage, QPixmap, QColor, QPainter, QPen, QFont, QTransform, QKeySequence
-from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtCore import Qt, QPoint, QRect
 
 
-class Window(QMainWindow):
+class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.setGeometry(0, 0, 1280, 720)
-        self.setFont(QFont('Segoe UI', 12))
+        # self.setFont(QFont('Segoe UI', 12))
+
+        layout = QGridLayout()
+        self.setLayout(layout)
 
         # self.canvas = QImage(self.size(), QImage.Format_ARGB32_Premultiplied)
         # self.canvas.fill(QColor(255, 0, 0, alpha=0))
 
-        self.drawing = False
         self.currentLayer = 0
         self.lastMousePos = QPoint(0, 0)
 
-        self.coords = QLabel(self)
-        self.coords.setText('0 0')
-        self.coords.setGeometry(0, 0, 200, 32)
-
-        self.layerList = QListWidget(self)
-
-        self.newBitmapLayer = QPushButton(self)
-        self.newBitmapLayer.setText('Новый растровый слой')
-
-        self.preview = PreviewWindow(self)
-        self.preview.show()
-
-        self.layout = QGridLayout()
-        self.setLayout(self.layout)
-        self.layout.addWidget(self.newBitmapLayer, 0, 0)
-        self.layout.addWidget(self.layerList, 0, 1)
-        self.layout.addWidget(self.preview, 0, 2)
+        layout.addWidget(QPushButton(self), 0, 0)
+        layout.itemAtPosition(0, 0).widget().setText('0')
+        layout.addWidget(QPushButton('0'), 0, 1)
+        layout.addWidget(PreviewWindow(self), 1, 0, alignment=Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(QPushButton('3'), 1, 1)
+        layout.itemAtPosition(1, 1).widget().setText('3')
 
         self.show()
 
@@ -42,11 +32,12 @@ class Window(QMainWindow):
 class PreviewWindow(QWidget):
     def __init__(self, parent):
         super().__init__(parent)
-        self.setGeometry(10, 10, 800, 600)
+        self.setMinimumSize(800, 600)
 
         self.scene = QGraphicsScene(self)
         self.view = QGraphicsView(self)
         self.view.setScene(self.scene)
+        self.view.setMinimumSize(800, 600)
         # self.view.show()
 
         self.scene.addPixmap(QPixmap('checkerboard.png'))
