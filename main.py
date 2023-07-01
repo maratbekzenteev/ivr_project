a5e612e80f58982686f842fc546ff2efccd2a64a
marat <maratbekzenteev@yandex.ru>
2023-07-01 16:49:29 +0300

    added a BitmapLayer class stub

--- main.py
+++ main.py
@@ -20,14 +20,17 @@ class Window(QWidget):
         # self.canvas.fill(QColor(255, 0, 0, alpha=0))
 
         self.currentLayer = 0
-        self.lastMousePos = QPoint(0, 0)
+        # Будет перемещена в кастомный виджет растрового слоя
+        # self.lastMousePos = QPoint(0, 0)
+        self.resolution = 720, 720
 
-        self.layout.addWidget(QPushButton(self), 0, 0)
-        self.layout.itemAtPosition(0, 0).widget().setText('0')
+        self.layout.addWidget(QPushButton("Новый растровый слой", self), 0, 0)
         self.layout.addWidget(QListWidget(), 1, 0)
         self.layout.addWidget(PreviewWidget(self), 1, 1, alignment=Qt.AlignmentFlag.AlignCenter)
         self.layout.addWidget(QPushButton('3'), 0, 1)
         self.layout.setColumnStretch(1, 1)
+
+        self.layout.itemAtPosition(0, 0).widget().clicked.connect(self.newBitmapLayer)
 
         self.zoomInShortcut = QShortcut(QKeySequence('Ctrl+='), self)
         self.zoomInShortcut.activated.connect(self.zoomIn)
@@ -36,7 +39,7 @@ class Window(QWidget):
         self.zoomOutShortcut.activated.connect(self.zoomOut)
 
         self.scene = QGraphicsScene(self)
-        self.scene.addPixmap(QPixmap('checkerboard.png'))
+        self.scene.addPixmap(QPixmap('checkerboard.png').scaled(*self.resolution, aspectRatioMode=Qt.IgnoreAspectRatio))
         self.layout.itemAtPosition(1, 1).widget().setScene(self.scene)
 
         self.show()
@@ -54,10 +57,34 @@ class Window(QWidget):
                   self.layout.itemAtPosition(0, 1).geometry().width(),
                   self.layout.itemAtPosition(1, 0).geometry().height()))
 
+    def newBitmapLayer(self):
+        self.scene.addWidget(BitmapLayer(*self.resolution))
+        self.layout.itemAtPosition(1, 0).widget().addItem('Новый растровый слой')
+
 
 class PreviewWidget(QGraphicsView):
     def __init__(self, parent=None):
         super().__init__(parent)
+
+
+class BitmapLayer(QWidget):
+    def __init__(self, width, height):
+        super().__init__()
+        self.setMinimumSize(width, height)
+        self.setMaximumSize(width, height)
+        self.resolution = width, height
+        self.active = False
+
+        pal = self.palette()
+        pal.setBrush(QPalette.Window, QBrush(QColor(0, 0, 0, alpha=128), Qt.SolidPattern))
+        self.setPalette(pal)
+
+    def paintEvent(self, event):
+        qp = QPainter(self)
+        qp.setPen(Qt.blue)
+        qp.setBrush(QBrush(QColor(255, 0, 0), Qt.SolidPattern))
+        qp.drawLine(0, 0, *self.resolution)
+        qp.drawLine(0, self.resolution[1], self.resolution[0], 0)
 
 
 if __name__ == "__main__":
