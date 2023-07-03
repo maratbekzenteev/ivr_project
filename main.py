79e9fc22e3c03d826906c8424c8ef9aa76e3c26b
marat <maratbekzenteev@yandex.ru>
2023-07-03 16:09:48 +0300

    merged the "initial commit" functionality into the current class system

--- main.py
+++ main.py
@@ -16,9 +16,6 @@ class Window(QWidget):
         self.layout = QGridLayout()
         self.setLayout(self.layout)
 
-        # self.canvas = QImage(self.size(), QImage.Format_ARGB32_Premultiplied)
-        # self.canvas.fill(QColor(255, 0, 0, alpha=0))
-
         self.currentLayer = 0
         # Будет перемещена в кастомный виджет растрового слоя
         # self.lastMousePos = QPoint(0, 0)
@@ -70,12 +67,17 @@ class PreviewWidget(QGraphicsView):
 class BitmapLayer(QWidget):
     def __init__(self, width, height):
         super().__init__()
+
         self.setMinimumSize(width, height)
         self.setMaximumSize(width, height)
         self.resolution = width, height
+
         self.bitmap = QImage(self.size(), QImage.Format_ARGB32_Premultiplied)
         self.bitmap.fill(QColor(0, 0, 0, alpha=0))
+
         self.active = False
+        self.drawing = False
+        self.lastMousePos = QPoint(0, 0)
 
         pal = self.palette()
         pal.setBrush(QPalette.Window, QBrush(QColor(0, 0, 0, alpha=128), Qt.SolidPattern))
@@ -84,6 +86,25 @@ class BitmapLayer(QWidget):
     def paintEvent(self, event):
         qp = QPainter(self)
         qp.drawImage(0, 0, self.bitmap)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.drawing = True
+            self.lastMousePos = event.pos()
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() & Qt.LeftButton & self.drawing:
+            qp = QPainter(self.bitmap)
+            qp.setPen(QPen(QColor(0, 0, 0), 10, Qt.SolidLine, Qt.RoundCap, Qt.BevelJoin))
+
+            qp.drawLine(self.lastMousePos, event.pos())
+            self.lastMousePos = event.pos()
+
+            self.update()
+
+    def mouseReleaseEvent(self, event):
+        if event.button == Qt.LeftButton:
+            self.drawing = False
 
 
 if __name__ == "__main__":
